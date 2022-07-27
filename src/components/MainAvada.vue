8724be0a18e92036abe19466455a49ab4474f71c
SamueleLettieri <samuele2073@gmail.com>
2022-07-27 17:54:05 +0200

    fine main

--- src/components/MainAvada.vue
+++ src/components/MainAvada.vue
@@ -4,6 +4,7 @@
     <MainServiceSolution/>
     <MainTrasporto/>
     <MainTestimonials/>
+    <MainBottom/>
   </main>
 </template>
 
@@ -12,12 +13,14 @@ import MainTop from './MainTop.vue';
 import MainServiceSolution from './MainServiceSolution.vue';
 import MainTrasporto from './MainTrasporto.vue';
 import MainTestimonials from './MainTestimonials.vue';
+import MainBottom from './MainBottom.vue';
 export default {
     components: {
     MainTop,
     MainServiceSolution,
     MainTrasporto,
-    MainTestimonials
+    MainTestimonials,
+    MainBottom
 }
 }
 </script>
---
+++ src/components/MainBottom.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <div class="container">
+        <div class="text-center ms_box">
+            <h1 class="fw-bold ms_title">Request A Free Quote</h1>
+            <p class="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsam asperiores ratione error autem aut, eum fugiat, accusamus veniam iste pariatur tempora rem! Dolores, minus quaerat deleniti vero quod tempore?</p>
+            <div class="ms_box-link mt-4"><a class="ms_link fw-bold" href="#">CLICK HERE</a></div>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+section{
+    padding: 100px;
+    background-image: url(../assets/img/avada-movers-request-quote-background.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.ms_box{
+    margin: 0 auto;
+    width: 700px;
+}
+
+</style>
