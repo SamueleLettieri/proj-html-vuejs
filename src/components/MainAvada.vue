02e0e8428959a4ebb89cba6db876549e72cb6d24
SamueleLettieri <samuele2073@gmail.com>
2022-07-27 15:16:31 +0200

    fine terza sezione, aggiunto di sfondo e di box info

--- src/components/MainAvada.vue
+++ src/components/MainAvada.vue
@@ -2,17 +2,19 @@
   <main>
     <MainTop/>
     <MainServiceSolution/>
-    
+    <MainTrasporto/>
   </main>
 </template>
 
 <script>
 import MainTop from './MainTop.vue';
 import MainServiceSolution from './MainServiceSolution.vue';
+import MainTrasporto from './MainTrasporto.vue';
 export default {
     components: {
     MainTop,
-    MainServiceSolution
+    MainServiceSolution,
+    MainTrasporto
 }
 }
 </script>
---
+++ src/components/MainTrasporto.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+    <div class="container">
+        <div class="text-end">
+            <div class="ms_background-lima p-4 d-inline-block text-center">
+                <h6 class="fw-bold">WE REMOVE THE HEADACHE FROM MOVING</h6>
+                <h1 class="fw-bold">Move With Ease</h1>
+                <hr>
+                <p class="mt-4">Whith our super simple service we can take you from getting a quotes, right through to being in your new home with all of your belongings securely packed and safely delivered, even in the rooms of your choice.</p>
+                <div class="ms_box-link mt-4"><a class="ms_link fw-bold" href="#">GET A FREE QUOTE NOW</a></div>
+            </div>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+section{
+    padding: 200px;
+    background-image: url(../assets/img/avada-movers-movewithease-background-new.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: top;    
+
+}
+
+.ms_background-lima{
+    background-color: #69bf14;
+    border: 15px solid #559910;
+    color: white;
+    width: 500px;
+}
+h6{
+    font-size: 12px;
+}
+
+hr{
+    margin-left: 50px;
+    margin-right: 50px;
+    width: 300px;
+}
+
+</style>
